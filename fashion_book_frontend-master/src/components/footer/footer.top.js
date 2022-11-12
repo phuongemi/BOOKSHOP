252fb269c9d356f797117db19f6eb6d44fffd596
Ellie <nguyenthiphuonggg66@gmail.com>
2022-11-12 19:31:21 +0700

    Update footer.top.js

--- fashion_book_frontend-master/src/components/footer/footer.top.js
+++ fashion_book_frontend-master/src/components/footer/footer.top.js
@@ -4,19 +4,17 @@ const FooterTop = () => (
     <div className="footer-top">
         <div className="container">
             <div className="row">
-                <div className="col-sm-3">
+                <div className="col-sm-6">
                     <div className="companyinfo">
                         <h2><span>BookShop</span></h2>
-                        <p>Bookshop là một website chuyên bán sách, bao gồm nhiều thể loại đa dạng và phong phú 
-                            , chúng tôi muốn cung cấp các trải nhiệm tuyệt vời nhất đên khách hàng
+                        <p>Bookshop là một website chuyên bán sách, bao gồm nhiều thể loại đa dạng và phong phú. 
+                        Chúng tôi muốn mang đến cho khách hàng những cuốn sách với nội dung hay và giá cả hợp lí. 
+                        Đồng thời, chúng tôi cũng muốn cung cấp các trải nhiệm tuyệt vời nhất đến với khách hàng.
                         </p>
                     </div>
                 </div>
-                <div className="col-sm-6"></div>
-          
-                <div className="col-sm-3">
-                    
-                </div>
+                <div className="col-sm-3"></div>         
+                <div className="col-sm-3"></div>
             </div>
         </div>
     </div>
